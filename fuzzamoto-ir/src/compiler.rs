c7e4cf4c12bcd4532b2c4bc8f9cb465e64f12c10
Eugene Siegel <elzeigel@gmail.com>
2025-10-31 08:07:10 -0400

    fuzzamoto-ir: wrap-around for output_selector in TakeTxo
    If the number of txos is equal to 0, call append_variable with a default
    Txo. Otherwise, wrap-around when output_selector is at the limit.

--- fuzzamoto-ir/src/compiler.rs
+++ fuzzamoto-ir/src/compiler.rs
@@ -88,6 +88,21 @@ struct Txo {
     value: u64,
 }
 
+impl Txo {
+    pub fn new() -> Self {
+        Self {
+            prev_out: ([0u8; 32], 0),
+            scripts: Scripts {
+                script_pubkey: Vec::new(),
+                script_sig: Vec::new(),
+                witness: Witness { stack: Vec::new() },
+                requires_signing: None,
+            },
+            value: 0,
+        }
+    }
+}
+
 #[derive(Clone)]
 struct TxOutputs {
     outputs: Vec<(Scripts, u64)>,
@@ -508,11 +523,16 @@ impl Compiler {
                 mut_tx_outputs_var.fees -= amount;
             }
             Operation::TakeTxo => {
-                let txo = {
-                    let tx_var = self.get_input_mut::<Tx>(&instruction.inputs, 0)?;
-                    tx_var.output_selector += 1; // TODO: wrap around?
-                    tx_var.txos[tx_var.output_selector - 1].clone()
-                };
+                let tx_var = self.get_input_mut::<Tx>(&instruction.inputs, 0)?;
+                let num_txos = tx_var.txos.len();
+                let mut txo = Txo::new();
+                if num_txos != 0 {
+                    tx_var.output_selector += 1;
+                    if num_txos == tx_var.output_selector - 1 {
+                        tx_var.output_selector = 1;
+                    }
+                    txo = tx_var.txos[tx_var.output_selector - 1].clone();
+                }
 
                 self.append_variable(txo);
             }
