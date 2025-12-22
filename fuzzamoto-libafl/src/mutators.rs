a98c85b386ee7a013264263bcff946946eadde6e
Eugene Siegel <elzeigel@gmail.com>
2025-12-22 17:27:35 -0500

    ir + libafl: add IncrementalSnapshotStage that wraps mutators, takes tmp snapshots
    The IncrementalSnapshotStage is configured to run with a tmp
    snapshot for 50 iterations. The mutators have awareness of the
    snapshot position.

--- fuzzamoto-libafl/src/mutators.rs
+++ fuzzamoto-libafl/src/mutators.rs
@@ -75,11 +75,15 @@ where
             None
         };
 
+        let min_index = input.frozen_prefix_len.unwrap_or(0);
+
         Ok(
-            match self
-                .mutator
-                .mutate(input.ir_mut(), &mut self.rng, tc_data.as_deref())
-            {
+            match self.mutator.mutate_from(
+                input.ir_mut(),
+                &mut self.rng,
+                tc_data.as_deref(),
+                min_index,
+            ) {
                 Ok(_) => MutationResult::Mutated,
                 _ => MutationResult::Skipped,
             },
@@ -149,10 +153,12 @@ where
 
         let other = other_testcase.load_input(state.corpus())?;
 
+        let min_index = input.frozen_prefix_len.unwrap_or(0);
+
         let mut input_clone = input.clone();
         if self
             .mutator
-            .splice(input_clone.ir_mut(), other.ir(), &mut self.rng)
+            .splice_from(input_clone.ir_mut(), other.ir(), &mut self.rng, min_index)
             .is_err()
         {
             return Ok(MutationResult::Skipped);
@@ -231,6 +237,12 @@ where
         else {
             return Ok(MutationResult::Skipped);
         };
+
+        let min_index = input.frozen_prefix_len.unwrap_or(0);
+
+        if index < min_index {
+            return Ok(MutationResult::Skipped);
+        }
 
         let mut builder = fuzzamoto_ir::ProgramBuilder::new(input.ir().context.clone());
 
